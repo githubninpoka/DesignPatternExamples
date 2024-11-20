3fdba5da5fa31d7e7decca3bc1bcd028b3b99377
githubninpoka <github@ninpoka.nl>
2024-11-20 10:24:00 +0100

    added some messing with NorthwindDb for grouping and lookup

---
+++ EfCoreAndNorthWindDb/DataAccess/NorthwindDb.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EfCoreAndNorthWindDb.Models; // To use DbContext and so on.
+
+namespace EfCoreAndNorthWindDb.DataAccess
+{
+    public class NorthwindDb : DbContext
+    {
+        public DbSet<Product>? Products { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string databaseFile = ".\\Data\\Northwind.db";
+            string path = Path.Combine(
+              Environment.CurrentDirectory, databaseFile);
+            string connectionString = $"Data Source={path}";
+            Console.WriteLine($"Connection: {connectionString}");
+            optionsBuilder.UseSqlite(connectionString);
+#if DEBUG
+            optionsBuilder.EnableSensitiveDataLogging(false);
+            optionsBuilder.EnableDetailedErrors(false);
+            //optionsBuilder.LogTo(Console.WriteLine);
+#endif
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Example of using Fluent API instead of attributes
+            // to limit the length of a category name to 15.
+            modelBuilder.Entity<Category>()
+              .Property(category => category.CategoryName)
+              .IsRequired() // NOT NULL
+              .HasMaxLength(15);
+            // Some SQLite-specific configuration.
+            if (Database.ProviderName?.Contains("Sqlite") ?? false)
+            {
+                // To "fix" the lack of decimal support in SQLite.
+                modelBuilder.Entity<Product>()
+                  .Property(product => product.Cost)
+                  .HasConversion<double>();
+                modelBuilder.Entity<Product>()
+                    .Property(product => product.ProductName)
+                    .IsRequired()
+                    .HasMaxLength(40);
+                modelBuilder.Entity<Product>()
+                    .Property(product => product.Stock)
+                    .HasMaxLength(20);
+                
+            }
+            // A global filter to remove discontinued products.
+            modelBuilder.Entity<Product>()
+              .HasQueryFilter(p => !p.Discontinued);
+        }
+
+        
+    }
+}
---
+++ EfCoreAndNorthWindDb/Program.cs
@@ -0,0 +1,172 @@
+﻿using EfCoreAndNorthWindDb.DataAccess;
+using EfCoreAndNorthWindDb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreAndNorthWindDb;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        using var db = new NorthwindDb();
+        Console.WriteLine(db.Database.ProviderName);
+        Console.WriteLine(db.Categories.FirstOrDefault().CategoryName);
+        //QueryingProducts();
+        //QueryingWithLike();
+        //GetProductUsingSql();
+        //JoinCategoriesAndProductsOrdered();
+        //GroupJoinCategoriesAndProducts();
+        ProductsLookup();
+    }
+
+    private static void QueryingProducts()
+    {
+        using NorthwindDb db = new();
+        Console.WriteLine("Products that cost more than a price, highest at top");
+        string? input;
+        decimal price;
+        do
+        {
+            Console.WriteLine("Enter a product price: ");
+            input = Console.ReadLine();
+        } while (!decimal.TryParse(input, out price));
+        IQueryable<Product>? products = db.Products?
+          .Where(product => product.Cost > price)
+          .OrderByDescending(product => product.Cost);
+        if (products is null || !products.Any())
+        {
+            Console.WriteLine("None found");
+            return;
+        }
+        //Console.WriteLine("Query executed:");
+        //Console.WriteLine(products.ToQueryString()); // works only on Iqueryables
+        foreach (Product p in products)
+        {
+            Console.WriteLine(
+              "{0}: {1} costs {2:$#,##0.00} and has {3} in stock.",
+              p.ProductId, p.ProductName, p.Cost, p.Stock);
+        }
+    }
+    private static void QueryingWithLike()
+    {
+        using NorthwindDb db = new();
+        Console.WriteLine("Pattern matching with LIKE");
+        Console.WriteLine("Enter part of a product name: ");
+        string? input = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("You did not enter part of a product name.");
+            return;
+        }
+        IQueryable<Product>? products = db.Products?
+          .Where(p => EF.Functions.Like(p.ProductName, $"%{input}%"));
+        if (products is null || !products.Any())
+        {
+            Console.WriteLine("No products found.");
+            return;
+        }
+        foreach (Product p in products)
+        {
+            Console.WriteLine("{0} has {1} units in stock. Discontinued: {2}",
+              p.ProductName, p.Stock, p.Discontinued);
+        }
+    }
+
+    private static void GetProductUsingSql()
+    {
+        using NorthwindDb db = new();
+        Console.WriteLine("Get product using SQL");
+        int? rowCount = db.Products?.Count();
+        if (rowCount is null)
+        {
+            Console.WriteLine("Products table is empty.");
+            return;
+        }
+        int productId = 1;
+        Product? p = db.Products?.FromSql(
+          $"SELECT * FROM Products WHERE ProductId = {productId}").FirstOrDefault();
+        if (p is null)
+        {
+            Console.WriteLine("Product not found.");
+            return;
+        }
+        Console.WriteLine($"Product: {p.ProductId} - {p.ProductName}");
+    }
+
+    private static void JoinCategoriesAndProductsOrdered()
+    {
+        Console.WriteLine("Join categories and products");
+        using NorthwindDb db = new();
+        // Join every product to its category to return 77 matches.
+        var queryJoin = db.Categories.Join(
+          inner: db.Products,
+          outerKeySelector: category => category.CategoryId,
+          innerKeySelector: product => product.CategoryId,
+          resultSelector: (c, p) =>
+            new { c.CategoryName, p.ProductName, p.ProductId })
+            .OrderBy(cp => cp.CategoryName); 
+        foreach (var p in queryJoin)
+        {
+            Console.WriteLine($"{p.ProductId}: {p.ProductName} in {p.CategoryName}.");
+        }
+    }
+
+    private static void GroupJoinCategoriesAndProducts()
+    {
+        Console.WriteLine("Group join categories and products");
+        using NorthwindDb db = new();
+        // Group all products by their category to return 8 matches.
+        var queryGroup = db.Categories.AsEnumerable().GroupJoin(
+          inner: db.Products,
+          outerKeySelector: category => category.CategoryId,
+          innerKeySelector: product => product.CategoryId,
+          resultSelector: (c, matchingProducts) => new
+          {
+              c.CategoryName,
+              Products = matchingProducts.OrderBy(p => p.ProductName)
+          });
+        foreach (var c in queryGroup)
+        {
+            Console.WriteLine($"{c.CategoryName} has {c.Products.Count()} products.");
+            foreach (var product in c.Products)
+            {
+                Console.WriteLine($"  {product.ProductName}");
+            }
+        }
+    }
+
+    private static void ProductsLookup()
+    {
+        Console.WriteLine("Products lookup");
+        using NorthwindDb db = new();
+        // Join all products to their category to return 77 matches.
+        var productQuery = db.Categories.Join(
+          inner: db.Products,
+          outerKeySelector: category => category.CategoryId,
+          innerKeySelector: product => product.CategoryId,
+          resultSelector: (c, p) => new { c.CategoryName, Product = p });
+        ILookup<string, Product> productLookup = productQuery.ToLookup(
+          keySelector: cp => cp.CategoryName,
+          elementSelector: cp => cp.Product);
+        foreach (IGrouping<string, Product> group in productLookup)
+        {
+            // Key is Beverages, Condiments, and so on.
+            Console.WriteLine($"{group.Key} has {group.Count()} products.");
+            foreach (Product product in group)
+            {
+                Console.WriteLine($" {product.ProductName}");
+            }
+        }
+        // We can look up the products by a category name.
+        Console.WriteLine("Enter a category name: ");
+        string categoryName = Console.ReadLine()!;
+        Console.WriteLine();
+        Console.WriteLine($"Products in {categoryName}:");
+        IEnumerable<Product> productsInCategory = productLookup[categoryName];
+        foreach (Product product in productsInCategory)
+        {
+            Console.WriteLine($"  {product.ProductName}");
+        }
+    }
+
+}

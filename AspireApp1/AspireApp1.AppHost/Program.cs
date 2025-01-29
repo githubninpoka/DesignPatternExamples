bd87c77db93d5c71e33c5ef0c91d5d9f0a6f21d3
Ninpoka <github.com@ninpoka.nl>
2025-01-29 11:37:14 +0100

    added a quick demo for working with tooltip in blazor

---
+++ AspireApp1/AspireApp1.AppHost/Program.cs
@@ -0,0 +1,14 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.AspireApp1_ApiService>("apiservice");
+
+builder.AddProject<Projects.AspireApp1_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService)
+    .WaitFor(apiService);
+
+builder.AddProject<Projects.BlazorPopover>("blazorpopover");
+
+builder.AddProject<Projects.BlazorTooltip>("blazortooltip");
+
+builder.Build().Run();
